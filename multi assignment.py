563c4086aee3823fdf59eda17d46071de539e949
Chris Francis <francis.chris5@gmail.com>
2024-01-24 20:41:06 -0500

    a bunch of collection stuff

---
+++ multi assignment.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 24 18:13:43 2024
+
+Created for SDEV 220 Fall 2024
+
+@author: c.s.francis
+
+"""
+
+a, b = 3, 4
+
+print(b)
+
+c = a**2 + b**2
+
+print(c)
+
+
+a, b = b, a
+
+print(b)
